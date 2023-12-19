c2807ee3f4a890ce867ccdedc8962f1ff2174c61
Nathan Turkson <nathanturkson@gmail.com>
2023-12-19 08:42:25 +0000

    card styling complete

--- advice-generator-app/src/app/components/AdviceCard.tsx
+++ advice-generator-app/src/app/components/AdviceCard.tsx
@@ -4,16 +4,19 @@ import IconDivider from "../../../public/images/pattern-divider-mobile.svg";
 import BookmarkIcon from "../../../public/images/icon-dice.svg";
 function AdviceCard() {
   return (
-    <article className="m-2 p-2 border-2 border-green-500 max-w-[45rem] flex justify-center flex-col items-center space-y-6">
-      <h2>Advice #117</h2>
-      <h1 className="">
-        It is easy to sit up and take notice, what’s difficult is getting up and
-        taking action.”
-      </h1>
+    <article className="m-4 py-10 px-4 rounded-[40px] max-w-[45rem] flex justify-center flex-col items-center  bg-gradient-to-r from-[#675a9b] to-[#000]">
+      <div className="flex flex-col  items-center justify-between space-y-10">
+        <h2 className="text-emerald-300 text-xl font-normal  tracking-[3px] italic uppercase" >
+          Advice #117
+        </h2>
+        <h1 className="text-white text-lg font-light text-center">
+          " It is easy to sit up and take notice, what’s difficult is getting up
+          and taking action.”
+        </h1>
 
-      <Image src={IconDivider} alt="Divider Icon" />
-
-      <div>
+        <Image src={IconDivider} alt="Divider Icon" />
+      </div>
+      <div className="relative  bg-secondary p-6 rounded-full top-[4.6rem]">
         <Image src={BookmarkIcon} alt="Bookmark icon" />
       </div>
     </article>
--- advice-generator-app/src/app/page.tsx
+++ advice-generator-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import AdviceCard from "./components/AdviceCard";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex justify-center items-center border-2 border-red-600">
+    <main className="min-h-screen flex justify-center items-center border-2 border-red-600 bg-gradient-to-r from-[#1F2531] to-[#4f547d] ">
       <AdviceCard />
     </main>
   );
