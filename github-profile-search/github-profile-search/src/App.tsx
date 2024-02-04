397c2c080157818d08523ad2c299e0c9ede258f5
Nathan Turkson <nathanturkson@gmail.com>
2024-02-04 20:47:11 +0000

    finsihed search input

--- github-profile-search/github-profile-search/src/App.tsx
+++ github-profile-search/github-profile-search/src/App.tsx
@@ -3,10 +3,10 @@ import Header from "./componets/Header"
 
 const App = () => {
   return (
-    <div className="p-4 font-spaceMono">
-     <Header /> 
+    <div className="p-4 font-spaceMono max-w-screen-md mx-auto">
+      <Header />
     </div>
-  )
+  );
 }
 
 export default App
--- github-profile-search/github-profile-search/src/componets/Header.tsx
+++ github-profile-search/github-profile-search/src/componets/Header.tsx
@@ -1,10 +1,13 @@
 
 import Navbar from "./Navbar.tsx";
+import SearchInput from "./SearchInput.tsx";
 
 const Header = () => {
   return (
     <header>
           <Navbar />
+          <SearchInput />
+
     </header>
   )
 }
